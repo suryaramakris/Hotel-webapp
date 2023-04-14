3f02c7dc5039607c085e5b7fff83f62dd49ca11f
Soorya R <90691113+suryaramakris@users.noreply.github.com>
2023-04-14 11:22:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.write("#")
 col1,col2 = st.columns(2)
 data = col1.selectbox("Please choose your locality :",options=("HSR Layout",'Jayanagar','MG Road',"Residency Road"))
 df = pd.read_csv(data+".csv")
-
+dfraw = pd.read_csv(data+".csv")
 #unique cuisine based on the locality
 food = []
 i=0
@@ -55,19 +55,10 @@ load1 = st.text("Loading "+data+" data...")
 time.sleep(0.3)
 load1.text("Loading "+data+" data...Done!!")
 
-#displaying the raw data
-st.subheader('Raw data after performing webscraping')
-st.write(df.head())
-st.text("Number of rows in the data "+str(df.shape[0])+' and columns '+str(df.shape[1]))
-load1.text("")
-
-#displaying the data after data cleaning
-st.subheader("Data after performing data cleaning!!")
-
-preprocess = st.text("Dropping unwanted column from the data!!")
+#preprocess = st.text("Dropping unwanted column from the data!!")
 df.drop("Unnamed: 0",axis=1,inplace=True)
 
-preprocess.text("Coverting the ratings column to int dtype!!")
+#preprocess.text("Coverting the ratings column to int dtype!!")
 def intcon(x):
     try:
         return float(x)
@@ -75,17 +66,17 @@ def intcon(x):
         return 0
 df["ratings"] = df['ratings'].apply(lambda x: intcon(x))
 
-preprocess.text("Removing text from cost column and converting to int dtype!!")
+#preprocess.text("Removing text from cost column and converting to int dtype!!")
 def costcon(x):
     return int(x[1:4])
 df['cost'] = df['cost'].apply(lambda x: costcon(x))
 
-preprocess.text("Removing 'min' from delivery minutes column!!")
+#preprocess.text("Removing 'min' from delivery minutes column!!")
 def deliverycon(x):
     return int(x[:2])
 df['deliveryin_min'] = df['deliveryin_min'].apply(lambda x: deliverycon(x))
 
-preprocess.text("Retaining number of orders only from order column!!")
+#preprocess.text("Retaining number of orders only from order column!!")
 def ordercon(x):
     try:
         a = x.find("+")
@@ -93,9 +84,6 @@ def ordercon(x):
     except:
         return 0
 df['order'] = df['order'].apply(lambda x: ordercon(x))
-
-preprocess.text("Done with data pre-processing!!")
-st.write(df.head())
 
 #to choose rows with cuisine selected by user
 if len(cuisine) !=0:
@@ -114,8 +102,8 @@ st.text(str(df.shape[0])+" restaurants matches your requirment")
 
 #sidebar for feature selection to display on plot
 st.sidebar.title("Select the columns and graph you desire to visualize")
-col1 = st.sidebar.selectbox("First columns :",options=("deliveryin_min",'cost','ratings','order'))
-col2 = st.sidebar.selectbox("Second columns :",options=("order",'cost','deliveryin_min','ratings'))
+col1 = st.sidebar.selectbox("First columns :",options=('cost',"deliveryin_min",'ratings','order'))
+col2 = st.sidebar.selectbox("Second columns :",options=('ratings','deliveryin_min','cost',"order"))
 
 #plot selection
 grp = st.sidebar.selectbox("First columns :",options=("scatter",'bar'))
@@ -129,3 +117,15 @@ elif grp == "bar":
     fig = plt.figure()
     plt.bar(df[col1],df[col2],color='tomato',edgecolor='black')
     st.write(fig)
+
+#displaying the raw data
+st.subheader('Raw data after performing webscraping')
+st.write(dfraw.head())
+st.text("Number of rows in the data "+str(df.shape[0])+' and columns '+str(df.shape[1]))
+load1.text("")
+
+#displaying the data after data cleaning
+st.subheader("Data after performing data cleaning!!")
+
+#preprocess.text("")
+st.write(df.head())
